12486d0b09f1d01f9d918e3137a4b4226289ce26
Shelby Wilson <s.wilson024@gmail.com>
2026-01-28 17:36:28 -0800

    mobile styling

--- ui/src/app/components/TempLandingPage.tsx
+++ ui/src/app/components/TempLandingPage.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./temp-landing.module.scss";
 import { Homepage } from "@/sanity/lib/utils";
+import LogoStacked from "./LogoStacked";
 
 export default function TempLandingPage({ content }: { content: Homepage }) {
   const time = new Date(content._updatedAt);
@@ -13,14 +14,17 @@ export default function TempLandingPage({ content }: { content: Homepage }) {
   return (
     <div className={styles.page}>
       <div className={`${styles.title}`}>
-        <Image
+        {/* <Image
           src="/RTRF_Logo-01_Stacked_BLK.png"
           width="3525"
           height="1280"
           alt="Rising Tide Research Foundation logo"
           className={` ${styles.panel}`}
           loading={"eager"}
-        />
+        /> */}
+        <div className={`${styles.logo}`}>
+          <LogoStacked />
+        </div>
         <p>{content.description}</p>
       </div>
 

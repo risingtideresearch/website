9bd887555f06c96508c2d30898965c41f62cfd42
Shelby Wilson <s.wilson024@gmail.com>
2026-02-27 11:55:51 -0800

    fix prop

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,14 +1,13 @@
 
 import Home from "./components/Home";
 import styles from "./page.module.scss";
-import { fetchHomepage, fetchLastUpdated } from "@/sanity/lib/utils";
+import { fetchHomepage } from "@/sanity/lib/utils";
 
 export default async function App() {
   const { data } = await fetchHomepage();
-  const updated = await fetchLastUpdated();
   return (
     <div className={styles.page}>
-      <Home content={data} lastUpdated={updated} />
+      <Home content={data} />
     </div>
   );
 }

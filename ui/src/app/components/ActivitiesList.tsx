bf3077374860927055987230ca23ce6787fe8436
Shelby Wilson <s.wilson024@gmail.com>
2026-03-05 13:53:05 -0800

    fade "in progress"

--- ui/src/app/components/ActivitiesList.tsx
+++ ui/src/app/components/ActivitiesList.tsx
@@ -1,5 +1,5 @@
 import { fetchActivities } from "@/sanity/lib/utils";
-import styles from  './activities.module.scss';
+import styles from './activities.module.scss';
 import { formatDate } from "../util";
 
 export default async function ActivitiesList() {
@@ -7,61 +7,34 @@ export default async function ActivitiesList() {
   return (
     <section>
       <h3>Our Activities</h3>
-      <ul style={{ listStyle: 'none'}}>
+      <ul className={styles.list}>
         {data
           .sort((a, b) => b.name.localeCompare(a.name))
           .map((activity, i) => (
-            <li style={{ position: "relative", margin: '1rem 0 3rem 0' }} key={activity._id}>
-              {/* {i == 0 ? (
-                <img
-                  style={{
-                    position: "absolute",
-                    top: "-2rem",
-                    left: "-2rem",
-                    maxWidth: "17rem",
-                    transform: "translate(-100%, 0)",
-                  }}
-                  src="/solander-drawing-2.png"
-                />
-              ) : (
-                ""
-              )} */}
+            <li className={styles.activity} key={activity._id}>
               <div className={styles.activities__title}>
                 <h4>{activity.name}</h4>
-                <div className={styles.activities__title__line}
-                ></div>
+                <div className={styles.activities__title__line}></div>
                 <p>{activity.description}</p>
               </div>
               <ol key={activity._id}>
                 {activity.programs
                   .sort((a, b) => a.name.localeCompare(b.name))
                   .map((program, j) => (
-                    <li
-                      key={program._id}
-                      style={{
-                        display: "grid",
-                        gridTemplateColumns: "2rem 1fr",
-                        rowGap: "1rem",
-                        columnGap: "2rem",
-                        margin: '1rem 0',
-                      }}
-                    >
+                    <li key={program._id} className={styles.program}>
                       <p>
                         {i + 1}&ndash;{String.fromCharCode(j + 65)}
                       </p>
-                      <div
-                        style={{
-                          display: "flex",
-                          justifyContent: "space-between",
-                        }}
-                      >
+                      <div className={styles.program__header}>
                         <p>
                           {program.content && program.content.length > 0
                             ? <a href={`/${program.slug?.current}`}>{program.name}</a>
                             : program.name}
                         </p>
                         <h3>
-                          {program.content ? formatDate(program._updatedAt) : <em>In progress</em>}
+                          {program.content
+                            ? formatDate(program._updatedAt)
+                            : <em className={styles.in_progress}>In progress</em>}
                         </h3>
                       </div>
                       <p></p>

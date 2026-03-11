2096e69369d8f549d5a775b591d32d858d992006
Shelby Wilson <s.wilson024@gmail.com>
2026-03-11 14:06:34 -0700

    remove date from footer

--- ui/src/app/components/Footer.tsx
+++ ui/src/app/components/Footer.tsx
@@ -1,12 +1,8 @@
-import { fetchLastUpdated } from "@/sanity/lib/utils";
 import Image from "next/image";
-import { formatDate } from "../util";
 import styles from "./footer.module.scss";
 import NewsletterForm from "./NewsletterForm";
 
 export default async function Footer() {
-  const updated = await fetchLastUpdated();
-
   return (
     <footer className={styles.footer}>
       <div className={styles.inner}>
@@ -22,15 +18,10 @@ export default async function Footer() {
             <h3>Connect with us</h3>
             <p>info@risingtideresearch.org</p>
           </div>
-          {/* <div>
-            <h3>Keep up-to-date with our latest work</h3>
-            <p>Subscribe to our newsletter</p>
-          </div> */}
           <div>
             <h3>Keep up-to-date with our latest work</h3>
             <NewsletterForm />
           </div>
-          <h3>Updated {formatDate(updated)}</h3>
         </div>
       </div>
     </footer>
